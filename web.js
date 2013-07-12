a11dc9660494853ecffc54d05372df01413c901e
rk662003 <rk@arkays.info>
2013-07-12 14:18:32 +0000

    Updatd web.js to show index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var infilr = "./index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync(infile);
+  var cont = content.toString();
+  response.send(cont);
 });
 
 var port = process.env.PORT || 5000;
